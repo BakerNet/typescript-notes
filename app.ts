862471ef2d4b5b2ef73abe939b64010e02d3ebab
BakerNet <hansbaker90@gmail.com>
2016-10-08 10:12:28 -0700

    more notes

---
+++ app.ts
@@ -0,0 +1,4 @@
+import * as Math from "./modules/circle";
+
+console.log(Math.PI);
+console.log(Math.calculateCircumference(10));
---
+++ build/app.js
@@ -0,0 +1,4 @@
+"use strict";
+const Math = require("./modules/circle");
+console.log(Math.PI);
+console.log(Math.calculateCircumference(10));
